dba33dcf39c92c0d9bee99341e5c451da4cd47da
Federico Brandani <federico.brandani@betbright.com>
2019-05-01 15:02:37 +0100

    Initial Commit

---
+++ Ryde/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ryde
+//
+//  Created by Federico Brandani on 01/05/2019.
+//  Copyright © 2019 EshvaSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
